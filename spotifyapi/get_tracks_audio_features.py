0aafce6669915d2cfb0140621d6e5739986ba77e
Scott Brown <scottdbrown7@gmail.com>
2023-12-16 19:55:44 -0700

    Update audio features

--- spotifyapi/get_tracks_audio_features.py
+++ spotifyapi/get_tracks_audio_features.py
@@ -29,15 +29,3 @@ def get_tracks_audio_features(track_ids: Union[str, list[str]], access_token: st
 
     audio_data = query_spotify(endpoint, access_token=access_token)    
     return audio_data
-
-
-    ## Define audio feature endpoint
-    # Changes if querying one or multiple tracks 
-    endpoint = (f'{AUDIO_FEATURES_ENDPOINT}/{track_ids}' if isinstance(track_ids, str) 
-                else f'{AUDIO_FEATURES_ENDPOINT}?ids={",".join(track_ids)}')
-
-    print(endpoint)
-    print('Getting audio feature data...')
-
-    audio_data = query_spotify(endpoint, access_token=access_token)    
-    return audio_data
